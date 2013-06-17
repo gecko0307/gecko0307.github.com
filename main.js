5cb85770e4f7758ee62acdbe285f374db8720bb6
gecko0307 <gecko0307@gmail.com>
2013-06-17 15:46:49 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-﻿function getDocumentName()
+function getDocumentName()
 {
     var file_name = document.location.href;
     var end = (file_name.indexOf("?") == -1) ? file_name.length : file_name.indexOf("?");
@@ -45,3 +45,109 @@ function drawSiteHeader()
     document.write('<div>Hello!</div>');
 }
 
+function getAvatar(service, userid, size)
+{
+    // this return the url that redirects to the according user image/avatar/profile picture
+    // implemented services: google profiles, facebook, gravatar, twitter, tumblr, default fallback
+    var url = '';
+ 
+    switch (service) {
+ 
+    case "google":
+        // available sizes: all, google rescales for you
+        url = "http://profiles.google.com/s2/photos/profile/" + userid + "?sz=" + size;
+        break;
+ 
+    case "facebook":
+        // available sizes: square (50x50), small (50xH) , normal (100xH), large (200xH)
+        var sizeparam = '';
+        if (isNumber(size)) {
+            if (size >= 200) {
+                sizeparam = 'large'
+            };
+            if (size >= 100 && size < 200) {
+                sizeparam = 'normal'
+            };
+            if (size >= 50 && size < 100) {
+                sizeparam = 'small'
+            };
+            if (size < 50) {
+                sizeparam = 'square'
+            };
+        } else {
+            sizeparam = size;
+        }
+        url = "https://graph.facebook.com/" + userid + "/picture?type=" + sizeparam;
+        break;
+ 
+    case "gravatar":
+        // available sizes: all, gravatar rescales for you
+        url = "http://www.gravatar.com/avatar/" + userid + "?s=" + size
+        break;
+ 
+    case "twitter":
+        // available sizes: bigger (73x73), normal (48x48), mini (24x24), 
+        // no param will give you full size
+        var sizeparam = '';
+        if (isNumber(size)) {
+            if (size >= 73) {
+                sizeparam = 'bigger'
+            };
+            if (size >= 48 && size < 73) {
+                sizeparam = 'normal'
+            };
+            if (size < 48) {
+                sizeparam = 'mini'
+            };
+        } else {
+            sizeparam = size;
+        }
+ 
+        url = "http://api.twitter.com/1/users/profile_image?screen_name=" + userid + "&size=" + sizeparam;
+        break;
+ 
+    case "tumblr":
+        // available sizes: 16, 24, 30, 40, 48, 64, 96, 128, 512
+        var sizeparam = '';
+        if (size >= 512) {
+            sizeparam = 512
+        };
+        if (size >= 128 && size < 512) {
+            sizeparam = 128
+        };
+        if (size >= 96 && size < 128) {
+            sizeparam = 96
+        };
+        if (size >= 64 && size < 96) {
+            sizeparam = 64
+        };
+        if (size >= 48 && size < 64) {
+            sizeparam = 48
+        };
+        if (size >= 40 && size < 48) {
+            sizeparam = 40
+        };
+        if (size >= 30 && size < 40) {
+            sizeparam = 30
+        };
+        if (size >= 24 && size < 30) {
+            sizeparam = 24
+        };
+        if (size < 24) {
+            sizeparam = 16
+        };
+ 
+        url = "http://api.tumblr.com/v2/blog/" + userid + "/avatar/" + sizeparam;
+        break;
+ 
+    default:
+        url = "http://i.imgur.com/RLiDK.png"; // 48x48
+    }
+
+    return url;
+}
+ 
+function isNumber(n)
+{
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
